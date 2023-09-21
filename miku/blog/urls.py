51aef9a451489a292c64724ea06de261e1070a00
Kenichi Kawaguchi <sonatine@pontalk.com>
2023-09-22 06:08:50 +0900

    add blog detail display

--- miku/blog/urls.py
+++ miku/blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('blog-detail/<int:pk>/', views.BlogDetail.as_view(), name="blog_detail"),
 ]
--- miku/blog/views.py
+++ miku/blog/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 
 from django.views import generic
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 
 
@@ -15,4 +15,10 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'orderby_records'
     queryset = BlogPost.objects.order_by('-posted_at')
+    paginate_by = 5
 
+
+class BlogDetail(DetailView):
+
+    template_name = 'blog/post.html'
+    model = BlogPost
