9346366ccdb295c02b4b1bb154bf4ef0e583c784
Kenichi Kawaguchi <sonatine@pontalk.com>
2023-09-22 06:59:08 +0900

    add user_type

--- miku/accounts/models.py
+++ miku/accounts/models.py
@@ -1,6 +1,17 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class UserType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class CustomUser(AbstractUser):
+    user_type = models.ForeignKey(UserType, on_delete=models.PROTECT, default=3)
     class Meta:
         verbose_name_plural = 'CustomUser'
+
+
