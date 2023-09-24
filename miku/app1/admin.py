03730ecb281e4caf7f2e9665ac0890171e91e58f
Kenichi Kawaguchi <sonatine@pontalk.com>
2023-09-24 11:03:49 +0900

    add LessonAdmin to app1/amin.py

--- miku/app1/admin.py
+++ miku/app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Lesson
 
-# Register your models here.
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('time', 'teacher', 'student', 'booking')
+
+
+admin.site.register(Lesson, LessonAdmin)
+
