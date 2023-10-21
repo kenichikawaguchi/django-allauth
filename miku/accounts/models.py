97609fb549990a808e88d4de7f171c051cf33c36
Kenichi Kawaguchi <sonatine@pontalk.com>
2023-10-21 09:13:04 +0900

    fix

--- miku/accounts/models.py
+++ miku/accounts/models.py
@@ -10,12 +10,8 @@ class UserType(models.Model):
 
 
 class CustomUser(AbstractUser):
-<<<<<<< HEAD
-    user_type = models.ForeignKey(UserType, on_delete=models.PROTECT, blank=True, null=True)
-=======
     user_type = models.ForeignKey(UserType, on_delete=models.PROTECT, null=True)
     email = models.EmailField(unique=True)
->>>>>>> main
     class Meta:
         verbose_name_plural = 'CustomUser'
 
